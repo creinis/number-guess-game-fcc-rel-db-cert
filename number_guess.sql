4fca77a14388e8cad4339329c5391c3f6a63f162
creinis <115181527+creinis@users.noreply.github.com>
2024-07-16 18:22:39 +0200

    feat: create db number guess, alter owner, connect to db

--- number_guess.sql
+++ number_guess.sql
@@ -21,7 +21,15 @@ DROP DATABASE number_guess;
 --
 -- Name: number_guess; Type: DATABASE; Schema: -; Owner: freecodecamp
 --
+CREATE DATABASE number_guess WITH 
+    TEMPLATE = template0 
+    ENCODING = 'UTF8' 
+    LC_COLLATE = 'C.UTF-8' 
+    LC_CTYPE = 'C.UTF-8';
 
+ALTER DATABASE number_guess OWNER TO freecodecamp;
+
+\connect number_guess
 
 
 SET statement_timeout = 0;
