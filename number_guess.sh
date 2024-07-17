75e39584eb56708ca73937366004c51d9439f3de
creinis <115181527+creinis@users.noreply.github.com>
2024-07-17 08:40:57 +0200

    feat: prompt the user for a username with Enter your username:, and take a username as input. Your database should allow usernames that are 22 characters

--- number_guess.sh
+++ number_guess.sh
@@ -4,3 +4,7 @@ PSQL="psql -X --username=freecodecamp --dbname=number_guess --tuples-only -c"
 
 echo -e "\n~~~~~~~~~~ Welcome to The Number Guessing Game ~~~~~~~~~~\n"
 
+# When you run your script, you should prompt the user for a username with Enter your username:, and take a username as input. Your database should allow usernames that are 22 characters
+echo -e "\nEnter your username:"
+read USERNAME
+
