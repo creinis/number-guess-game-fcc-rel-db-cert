11020e52cb1f27ba6b51e0914896d3a2c76ab119
creinis <115181527+creinis@users.noreply.github.com>
2024-07-17 08:41:26 +0200

    feat: If that username has been used before, it should print Welcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses., with <username> being a users name from the database, <games_played> being the total number of games that user has played, and <best_game> being the fewest number of guesses it took that user to win the game

--- number_guess.sh
+++ number_guess.sh
@@ -8,3 +8,7 @@ echo -e "\n~~~~~~~~~~ Welcome to The Number Guessing Game ~~~~~~~~~~\n"
 echo -e "\nEnter your username:"
 read USERNAME
 
+# If that username has been used before, it should print Welcome back, <username>! You have played <games_played> games, and your best game took <best_game> guesses., with <username> being a users name from the database, <games_played> being the total number of games that user has played, and <best_game> being the fewest number of guesses it took that user to win the game
+USERNAME_CHECK=$($PSQL "SELECT username FROM users WHERE username='$USERNAME'" )
+
+
